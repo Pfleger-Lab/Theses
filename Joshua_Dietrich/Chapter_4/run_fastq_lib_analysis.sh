c99d92e820e4f6e5065427589dc225499c39251c
jdietrich3 <69177938+jdietrich3@users.noreply.github.com>
2024-11-04 10:35:50 -0500

    remove files for now until deposit date

--- Joshua_Dietrich/Chapter_4/run_fastq_lib_analysis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source /opt/bifxapps/miniconda3/etc/profile.d/conda.sh
-unset PYTHONPATH
-conda activate /home/glbrc.org/jdietrich3/.conda/envs/biopython
-python fastq_library_seq_complete_determ_only_Scarcity_v4.py 2023_0725_unsorted_lib_pass.fastq
-conda deactivate
